126b3b1c7096a06320ea5be276392ea638137687
SteamedFish <i@steamedfish.org>
2020-02-01 15:44:18 +0800

    enable load

--- .p10k.zsh
+++ .p10k.zsh
@@ -84,7 +84,7 @@
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
     # vpn_ip                # virtual private network indicator
-    # load                  # CPU load
+    load                  # CPU load
     # disk_usage            # disk usage
     # ram                   # free RAM
     # swap                  # used swap
