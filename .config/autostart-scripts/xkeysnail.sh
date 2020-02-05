659cfcc87b41884c54d614c529c01ba36d64bf1b
SteamedFish <i@steamedfish.org>
2020-02-05 22:23:01 +0800

    Add exist detection

--- .config/autostart-scripts/xkeysnail.sh
+++ .config/autostart-scripts/xkeysnail.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-/usr/bin/sudo /usr/bin/xkeysnail --quiet --watch $HOME/.config/xkeysnail/config.py
+if [ -f /usr/bin/xkeysnail ] && [ -f "$HOME/.config/xkeysnail/config.py" ]; then
+    /usr/bin/sudo /usr/bin/xkeysnail --quiet --watch "$HOME/.config/xkeysnail/config.py"
+fi
