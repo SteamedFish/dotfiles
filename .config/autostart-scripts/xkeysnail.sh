ff83c0ee425763e808476bc858c4015ac14e3820
SteamedFish <i@steamedfish.org>
2020-05-09 19:29:19 +0800

    remove xkeysnails script

--- .config/autostart-scripts/xkeysnail.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -f /usr/bin/xkeysnail ] && [ -f "$HOME/.config/xkeysnail/config.py" ]; then
-    /usr/bin/sudo /usr/bin/xkeysnail --quiet --watch "$HOME/.config/xkeysnail/config.py"
-fi
