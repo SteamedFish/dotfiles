8ca8fe877776aac6b1b1f74b1ee11c0d0f5e4025
SteamedFish <i@steamedfish.org>
2020-10-23 12:18:29 +0800

    Revert "change orgroam directory"
    This reverts commit 062820084799a9255f1b38b00d204a0425aacda0.

--- .config/doom/modules/steamedfish/orgmode/config.el
+++ .config/doom/modules/steamedfish/orgmode/config.el
@@ -6,7 +6,7 @@
   (custom-set-faces!
    '(org-ellipsis :foreground nil))
   (setq org-directory (expand-file-name "~/work/org/")
-    org-roam-directory org-directory
+    org-roam-directory (file-name-as-directory (expand-file-name (concat org-directory "/roam")))
     org-roam-db-location (expand-file-name "org-roam.db" doom-cache-dir)
     diary-file (expand-file-name (concat org-directory "/diary"))
     ;; all files but later.org should be put in agenda
