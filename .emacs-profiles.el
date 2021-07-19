23a753d3d3f9b57763f4c78ea7d20ce0b3470c9e
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-07-19 15:57:16 +0800

    update chemacs profile location

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,10 +1,10 @@
 ;;; .emacs-profiles.el  -*- lexical-binding: t; -*-
 
 
-(("default" . ((user-emacs-directory . "~/.emacs.d")))
+(("default" . ((user-emacs-directory . "~/.config/emacs")))
  ("spacemacs" . ((user-emacs-directory . "~/emacs-distros/spacemacs")
                  (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
  ("doom" . ((user-emacs-directory . "~/emacs-distros/doom-emacs")
-            (env . (("DOOMDIR" . "~/.doom.d")))))
+            (env . (("DOOMDIR" . "~/.config/doom")))))
  ("test". ((user-emacs-directory . "~/.emacs.d-test")))
  ("centaur". ((user-emacs-directory . "~/emacs-distros/centaur"))))
