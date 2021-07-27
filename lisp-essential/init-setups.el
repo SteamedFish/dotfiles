9c04f1158c76d9e3e59fbb74efd7eee1423d8aa2
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-07-28 00:59:04 +0800

    manage with leaf

--- lisp-essential/init-setups.el
+++ lisp-essential/init-setups.el
@@ -7,11 +7,24 @@
 ;;; Code:
 
 
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(leaf cus-start
+  :doc "builtins"
+  :tag "builtin" "internal"
+  :ensure nil
+  :custom ((user-full-name . "SteamedFish")
+           (user-mail-address . "steamedfish@hotmail.com")
+           (frame-title-format . '("%b - " user-full-name "'s Emacs"))))
 
-(setq user-full-name "SteamedFish"
-      user-mail-address "steamedfish@hotmail.com"
-      frame-title-format '("%b - " user-full-name "'s Emacs"))
+(leaf cus-edit
+  :ensure nil
+  :tag "builtin" "internal"
+  :init
+  (custom-set-variables `(custom-file ,(concat my-data-dir "custom.el")))
+  :config
+  (add-hook 'after-init-hook
+    (lambda ()
+      (when (and (stringp custom-file) (file-exists-p custom-file))
+        (load custom-file)))))
 
 (provide 'init-setups)
 ;;; init-setups.el ends here
