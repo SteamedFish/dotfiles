752a27097855a05411aa1f6f88f4a4da7f4d85c0
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-07-28 00:55:54 +0800

    save data to another dir

--- init.el
+++ init.el
@@ -10,15 +10,15 @@
 (push (expand-file-name "lisp-essential" user-emacs-directory) load-path)
 (push (expand-file-name "lisp" user-emacs-directory) load-path)
 
+(defconst my-data-dir (concat user-emacs-directory ".local/")
+  "The root directory where I put data files")
+(unless (file-exists-p my-data-dir)
+  (make-directory my-data-dir))
 
 (require 'init-earlyinit)
 (require 'init-straight)
 (require 'init-leaf)
 (require 'init-setups)
-
-;; load custom-file
-(when (file-exists-p custom-file)
-  (load custom-file))
 
 (provide 'init)
 ;;; init.el ends here
