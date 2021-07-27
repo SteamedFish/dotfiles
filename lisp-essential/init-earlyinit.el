1e2d84056b1c1a88be83101119b67ed0ea156b4c
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-07-28 00:58:08 +0800

    remove gc hook

--- lisp-essential/init-earlyinit.el
+++ lisp-essential/init-earlyinit.el
@@ -28,13 +28,6 @@
 ;; Ignore X resources
 (advice-add #'x-apply-session-resources :override #'ignore)
 
-;; we don't need gc at startup
-;; after startup, set it to 20MB
-(setq gc-cons-threshold most-positive-fixnum)
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (setq gc-cons-threshold (* 20 1024 1024))))
-
 ;; Don't display useless messages
 (setq inhibit-startup-message t
       inhibit-startup-echo-area-message t
