de76c2ed697b48d7a4812f5689ef76dd596e62b5
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-08-06 18:45:47 +0800

    absolute data dir

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 (push (expand-file-name "lisp-tools" user-emacs-directory) load-path)
 (push (expand-file-name "lisp-lang" user-emacs-directory) load-path)
 
-(defconst my-data-dir (concat user-emacs-directory ".local/")
+(defconst my-data-dir (expand-file-name (concat user-emacs-directory ".local/"))
   "The root directory where I put data files")
 (unless (file-exists-p my-data-dir)
   (make-directory my-data-dir))
