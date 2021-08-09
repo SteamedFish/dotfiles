7014427896b39220bb05b96e15aca7c1a1bc61d7
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-08-10 01:52:00 +0800

    remove window-divider

--- lisp-theme/init-theme.el
+++ lisp-theme/init-theme.el
@@ -67,13 +67,13 @@
 ;;   :config (load-theme 'bespoke t))
 
 ;; Vertical window divider
-(leaf frame
-  :tag "builtin"
-  :global-minor-mode window-divider-mode
-  :custom
-  (window-divider-default-right-width . 12)
-  (window-divider-default-bottom-width . 1)
-  (window-divider-default-places . 'right-only))
+;; (leaf frame
+;;   :tag "builtin"
+;;   :global-minor-mode window-divider-mode
+;;   :custom
+;;   (window-divider-default-right-width . 12)
+;;   (window-divider-default-bottom-width . 1)
+;;   (window-divider-default-places . 'right-only))
 
 (leaf hl-todo
   :url "https://github.com/tarsius/hl-todo"
