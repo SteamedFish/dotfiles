ae2057c5c234e015cc19028d4d5edc5a6cde75d0
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-08-12 00:27:30 +0800

    don't always run system-packages-ensure

--- lisp-lang/init-golang.el
+++ lisp-lang/init-golang.el
@@ -18,44 +18,45 @@
   :url https://github.com/dominikh/go-mode.el
   :straight t
   :config
-  (cond
-   (IS-LINUX
-    (system-packages-ensure "go-tools"))
-   (IS-MAC
-    (unless (executable-find "guru")
-      (async-shell-command "GO111MODULE=on go get -u golang.org/x/tools/cmd/guru")))))
+  (unless (executable-find "guru")
+    (cond
+     (IS-LINUX (system-packages-ensure "go-tools"))
+     (IS-MAC (async-shell-command
+              "GO111MODULE=on go get -u golang.org/x/tools/cmd/guru")))))
 
 (leaf go-dlv
   :url https://github.com/benma/go-dlv.el
   :doc "Go Delve: Debug Go Programs with the GUD"
   :straight t
   :config
-  (cond
-   (IS-LINUX
-    (system-packages-ensure "delve"))
-   (IS-MAC
-    (unless (executable-find "dlv")
-      (async-shell-command "GO111MODULE=on go get -u github.com/go-delve/delve/cmd/dlv")))))
+  (unless (executable-find "dlv")
+    (cond
+     (IS-LINUX (system-packages-ensure "delve"))
+     (IS-MAC (async-shell-command
+              "GO111MODULE=on go get -u github.com/go-delve/delve/cmd/dlv")))))
 
 (leaf go-fill-struct
   :url https://github.com/s-kostyaev/go-fill-struct
   :straight t
   :config
   (unless (executable-find "fillstruct")
-    (async-shell-command "GO111MODULE=on go get -u github.com/davidrjenni/reftools/cmd/fillstruct")))
+    (async-shell-command
+     "GO111MODULE=on go get -u github.com/davidrjenni/reftools/cmd/fillstruct")))
 
 (leaf go-impl
   :url https://github.com/emacsorphanage/go-impl
   :straight t
   :config
-  (cond
-   (IS-LINUX
-    (system-packages-ensure "go-impl"))
-   (IS-MAC
-    (unless (executable-find "impl")
-      (async-shell-command "GO111MODULE=on go get -u github.com/josharian/impl@latest"))
-    (unless (executable-find "godoc")
-      (async-shell-command "GO111MODULE=on go get -u golang.org/x/tools/cmd/godoc")))))
+  (unless (executable-find "impl")
+    (cond
+     (IS-LINUX (system-packages-ensure "go-impl"))
+     (IS-MAC (async-shell-command
+              "GO111MODULE=on go get -u github.com/josharian/impl@latest"))))
+  (unless (executable-find "godoc")
+    (cond
+     (IS-LINUX (system-packages-ensure "go-tools"))
+     (IS-MAC (async-shell-command
+              "GO111MODULE=on go get -u golang.org/x/tools/cmd/godoc")))))
 
 (leaf flycheck-golangci-lint
   :url https://github.com/weijiangan/flycheck-golangci-lint
@@ -73,25 +74,28 @@
     (system-packages-ensure "gocode-daemon"))
    (IS-MAC
     (unless (executable-find "gocode")
-      (async-shell-command "GO111MODULE=on go get -u github.com/nsf/gocode")))))
+      (async-shell-command
+       "GO111MODULE=on go get -u github.com/nsf/gocode")))))
 
 (leaf go-tag
   :url https://github.com/brantou/emacs-go-tag
   :straight t
   :config
-  (cond
-   (IS-LINUX
-    (system-packages-ensure "gomodifytags"))
-   (IS-MAC
-    (unless (executable-find "gomodifytags")
-      (async-shell-command "GO111MODULE=on go get -u github.com/fatih/gomodifytags@latest")))))
+  (unless (executable-find "gomodifytags")
+    (cond
+     (IS-LINUX
+      (system-packages-ensure "gomodifytags"))
+     (IS-MAC
+      (async-shell-command
+       "GO111MODULE=on go get -u github.com/fatih/gomodifytags@latest")))))
 
 (leaf go-gen-test
   :url https://github.com/s-kostyaev/go-gen-test
   :straight t
   :config
   (unless (executable-find "gotests")
-    (async-shell-command "GO111MODULE=on go get -u github.com/cweill/gotests/...")))
+    (async-shell-command
+     "GO111MODULE=on go get -u github.com/cweill/gotests/...")))
 
 (leaf gotest
   :url https://github.com/nlamirault/gotest.el
