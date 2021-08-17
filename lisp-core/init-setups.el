65dea756adcdf23e6c0f385147ed631e2c73676c
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-08-17 23:06:50 +0800

    add more env to keep

--- lisp-core/init-setups.el
+++ lisp-core/init-setups.el
@@ -116,7 +116,9 @@
                                     "GOPROXY"
                                     "PYTHONPATH"
                                     "SHELL"
-                                    "SSH_AUTH_SOCK")))
+                                    "SSH_AUTH_SOCK"
+                                    "SSH_AGENT_PID"
+                                    "GPG_AGENT_INFO")))
 
 ;; remember last location
 (leaf saveplace
