acd03d58d4ed720e845f79519e8827dcbeda2dce
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-08-17 21:27:57 +0800

    configure lsp

--- lisp-tools/init-lsp.el
+++ lisp-tools/init-lsp.el
@@ -11,6 +11,11 @@
 (leaf lsp-mode
   :url https://emacs-lsp.github.io/lsp-mode/
   :straight t
+  :setq
+  (lsp-restart . 'auto-restart)
+  (lsp-enable-file-watchers . nil)
+  (lsp-eldoc-render-all . t)
+  (lsp-enable-semantic-highlighting . t)
   :hook
   ((prog-mode . (lambda ()
                   (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode)
@@ -22,7 +27,13 @@
 
 (leaf lsp-ui
   :url https://emacs-lsp.github.io/lsp-ui/
-  :straight t)
+  :straight t
+  :setq
+  (lsp-ui-doc-position . 'at-point)
+  (lsp-ui-flycheck-enable . t)
+  (lsp-ui-sideline-ignore-duplicate . t)
+  (lsp-ui-sideline-update-mode . 'point)
+  (lsp-ui-doc-enable . t))
 
 (leaf company-lsp
   :url https://github.com/tigersoldier/company-lsp
@@ -31,6 +42,23 @@
   :require t
   :config
   (push 'company-lsp company-backends))
+
+(leaf lsp-ivy
+  :url https://github.com/emacs-lsp/lsp-ivy
+  :straight t
+  :bind
+  (:lsp-mode-map
+   ([remap xref-find-apropos] . lsp-ivy-workspace-symbol)))
+
+(leaf dap-mode
+  :url https://emacs-lsp.github.io/dap-mode/
+  :straight t
+  :global-minor-mode dap-auto-configure-mode)
+
+(leaf lsp-treemacs
+  :url https://github.com/emacs-lsp/lsp-treemacs
+  :after treemacs
+  :global-minor-mode lsp-treemacs-sync-mode)
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
