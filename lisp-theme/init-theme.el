fe99f332e3996441adc2f634cd60cfab18425ae3
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-08-17 22:21:08 +0800

    disable with leaf's keywords

--- lisp-theme/init-theme.el
+++ lisp-theme/init-theme.el
@@ -47,80 +47,85 @@
   :when IS-GUI
   :straight (svg-tag-mode :host github :repo "rougier/svg-tag-mode"))
 
-;; (leaf nano
-;;   :url "https://github.com/rougier/nano-emacs"
-;;   :straight (nano :host github :repo "rougier/nano-emacs")
-;;   :pre-setq
-;;   (nano-font-family-monospaced . "Rec Mono Duotone")
-;;   (nano-font-family-proportional . "Fira Sans")
-;;   :require nano-base-colors
-;;   :require nano-colors
-;;   :require nano-faces
-;;   ;; :require nano-theme-dark
-;;   ;; :require nano-theme
-;;   :require nano-layout
-;;   ;; :require nano-modeline
-;;   ;; :require nano-colors
-;;   :require nano-minibuffer
-;;   ;; :require nano-counsel
-;;   ;; :require nano-mu4e
-;;   ;; :require nano-agenda
-;;   :config
-;;   ;; (nano-theme)
-;;   (nano-faces))
+(leaf nano
+  :url "https://github.com/rougier/nano-emacs"
+  :straight (nano :host github :repo "rougier/nano-emacs")
+  :disabled t
+  :pre-setq
+  (nano-font-family-monospaced . "Rec Mono Duotone")
+  (nano-font-family-proportional . "Fira Sans")
+  :require nano-base-colors
+  :require nano-colors
+  :require nano-faces
+  ;; :require nano-theme-dark
+  ;; :require nano-theme
+  :require nano-layout
+  ;; :require nano-modeline
+  ;; :require nano-colors
+  :require nano-minibuffer
+  ;; :require nano-counsel
+  ;; :require nano-mu4e
+  ;; :require nano-agenda
+  :config
+  ;; (nano-theme)
+  (nano-faces))
 
-;; (leaf bespoke-themes
-;;   :url "https://github.com/mclear-tools/bespoke-themes"
-;;   :straight (bespoke-themes :host github :repo "mclear-tools/bespoke-themes")
-;;   :pre-setq
-;;   ;; Set header line
-;;   (bespoke-set-mode-line . 'nil) ;; not working correctly
-;;   ;; Set mode line height
-;;   (bespoke-set-mode-line-size . 3)
-;;   ;; Show diff lines in modeline
-;;   (bespoke-set-git-diff-mode-line . t)
-;;   ;; Set mode-line cleaner
-;;   (bespoke-set-mode-line-cleaner . t)
-;;   ;; Set evil cursor colors
-;;   (bespoke-set-evil-cursors . t)
-;;   ;; Use mode line visual bell
-;;   (bespoke-set-visual-bell . t)
-;;   ;; Set use of italics
-;;   (bespoke-set-italic-comments . t)
-;;   (bespoke-set-italic-keywords . t)
-;;   ;; Set variable pitch
-;;   (bespoke-set-variable-pitch . t)
-;;   ;; Set initial theme variant
-;;   (bespoke-set-theme 'dark)
-;;   :config (load-theme 'bespoke t))
+(leaf bespoke-themes
+  :url "https://github.com/mclear-tools/bespoke-themes"
+  :straight (bespoke-themes :host github :repo "mclear-tools/bespoke-themes")
+  :disabled t
+  :pre-setq
+  ;; Set header line
+  (bespoke-set-mode-line . 'nil) ;; not working correctly
+  ;; Set mode line height
+  (bespoke-set-mode-line-size . 3)
+  ;; Show diff lines in modeline
+  (bespoke-set-git-diff-mode-line . t)
+  ;; Set mode-line cleaner
+  (bespoke-set-mode-line-cleaner . t)
+  ;; Set evil cursor colors
+  (bespoke-set-evil-cursors . t)
+  ;; Use mode line visual bell
+  (bespoke-set-visual-bell . t)
+  ;; Set use of italics
+  (bespoke-set-italic-comments . t)
+  (bespoke-set-italic-keywords . t)
+  ;; Set variable pitch
+  (bespoke-set-variable-pitch . t)
+  ;; Set initial theme variant
+  (bespoke-set-theme 'dark)
+  :config (load-theme 'bespoke t))
 
 ;; Vertical window divider
-;; (leaf frame
-;;   :tag "builtin"
-;;   :global-minor-mode window-divider-mode
-;;   :custom
-;;   (window-divider-default-right-width . 12)
-;;   (window-divider-default-bottom-width . 1)
-;;   (window-divider-default-places . 'right-only))
+(leaf frame
+  :tag "builtin"
+  :disabled t
+  :global-minor-mode window-divider-mode
+  :custom
+  (window-divider-default-right-width . 12)
+  (window-divider-default-bottom-width . 1)
+  (window-divider-default-places . 'right-only))
 
 (leaf hl-todo
   :url "https://github.com/tarsius/hl-todo"
   :straight t
   :global-minor-mode global-hl-todo-mode)
 
-;; (leaf nano-theme
-;;   :url "https://github.com/rougier/nano-theme"
-;;   :straight (nano-theme :host github :repo "rougier/nano-theme")
-;;   ;; :require nano-theme
-;;   ;; :config (nano-dark)
-;;   :config (load-theme 'nano t))
+(leaf nano-theme
+  :url "https://github.com/rougier/nano-theme"
+  :straight (nano-theme :host github :repo "rougier/nano-theme")
+  :disabled t
+  ;; :require nano-theme
+  ;; :config (nano-dark)
+  :config (load-theme 'nano t))
 
-;; (leaf nano-modeline
-;;   :url "https://github.com/rougier/nano-modeline"
-;;   :straight (nano-modeline :host github :repo "rougier/nano-modeline")
-;;   :custom
-;;   (nano-modeline-position . 'bottom)
-;;   :require (nano-modeline))
+(leaf nano-modeline
+  :url "https://github.com/rougier/nano-modeline"
+  :straight (nano-modeline :host github :repo "rougier/nano-modeline")
+  :disabled t
+  :custom
+  (nano-modeline-position . 'bottom)
+  :require (nano-modeline))
 
 (leaf nord-theme
   :url "https://github.com/arcticicestudio/nord-emacs"
