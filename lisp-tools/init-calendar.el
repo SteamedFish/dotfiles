df00261f4ff9a4c75f9655e8b6e557fed9a34688
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-08-30 15:18:35 +0800

    format

--- lisp-tools/init-calendar.el
+++ lisp-tools/init-calendar.el
@@ -11,11 +11,11 @@
 (leaf calendar
   :tag "builtin"
   :setq
-  '((calendar-latitude . 23.1247)
-    (calendar-longitude . 113.3612)
-    (calendar-location-name . "Tianhe, Guangzhou")
-    (calendar-mark-holidays-flag . t)
-    (calendar-mark-diary-entries-flag . t)))
+  (calendar-latitude . 23.1247)
+  (calendar-longitude . 113.3612)
+  (calendar-location-name . "Tianhe, Guangzhou")
+  (calendar-mark-holidays-flag . t)
+  (calendar-mark-diary-entries-flag . t))
 
 (leaf cal-china-x
   :url "https://github.com/xwl/cal-china-x"
@@ -27,7 +27,7 @@
   ;;                          cal-china-x-general-holidays)))
   :config
   (setq calendar-holidays (append cal-china-x-chinese-holidays
-                            cal-china-x-general-holidays)))
+                                  cal-china-x-general-holidays)))
 (leaf appt
   :tag "builtin"
   :unless noninteractive
