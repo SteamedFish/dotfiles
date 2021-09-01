4eccb56f2a68d92fb6a8572fd0cb35c96799f511
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-09-01 17:59:24 +0800

    add counsel-projectile

--- lisp-tools/init-projects.el
+++ lisp-tools/init-projects.el
@@ -27,5 +27,22 @@
     (when (file-directory-p dir)
       (add-to-list 'projectile-project-search-path (expand-file-name dir)))))
 
+(leaf counsel-projectile
+  :url https://github.com/ericdanan/counsel-projectile
+  :straight t
+  :bind
+  ;; counsel-projectile-git-grep
+  ;; counsel-projectile-org-capture
+  ;; counsel-projectile-org-agenda
+  ;; counsel-projectile
+  ([remap projectile-find-file] . counsel-projectile-find-file)
+  ([remap projectile-find-file-dwim] . counsel-projectile-find-file-dwim)
+  ([remap projectile-find-dir]  . counsel-projectile-find-dir)
+  ([remap projectile-switch-to-buffer]  . counsel-projectile-switch-to-buffer)
+  ([remap projectile-grep]  . counsel-projectile-grep)
+  ([remap projectile-ag]  . counsel-projectile-ag)
+  ([remap projectile-ripgrep]  . counsel-projectile-rg)
+  ([remap projectile-switch-project]  . counsel-projectile-switch-project))
+
 (provide 'init-projects)
 ;;; init-projects.el ends here
