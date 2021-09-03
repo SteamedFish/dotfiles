0b6fbef78dbdc06051a1320f77636a2f3390a958
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-09-03 19:02:07 +0800

    configure treemacs

--- lisp-theme/init-treemacs.el
+++ lisp-theme/init-treemacs.el
@@ -11,30 +11,36 @@
 
 (leaf treemacs
   :url https://github.com/Alexander-Miller/treemacs
-  :straight t)
+  :straight t
+  :setq
+  (treemacs-is-never-other-window . t))
 
 (leaf treemacs-evil
-  :url
+  :url https://github.com/Alexander-Miller/treemacs
   :straight t)
 
 (leaf treemacs-projectile
-  :url
+  :url https://github.com/Alexander-Miller/treemacs
   :straight t)
 
 (leaf treemacs-magit
-  :url
+  :url https://github.com/Alexander-Miller/treemacs
   :straight t)
 
 (leaf treemacs-icons-dired
-  :url
+  :url https://github.com/Alexander-Miller/treemacs
   :straight t)
 
 (leaf treemacs-perspective
-  :url
+  :url https://github.com/Alexander-Miller/treemacs
+  :straight t)
+
+(leaf treemacs-persp
+  :url https://github.com/Alexander-Miller/treemacs
   :straight t)
 
 (leaf treemacs-all-the-icons
-  :url
+  :url https://github.com/Alexander-Miller/treemacs
   :straight t)
 
 (provide 'init-treemacs)
