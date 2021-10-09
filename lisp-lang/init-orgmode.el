0c16622f452f05ab60e40db04c10e1bc1e2fb0b9
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-10-09 17:48:54 +0800

    advice save-buffer-kill-emacs instead

--- lisp-lang/init-orgmode.el
+++ lisp-lang/init-orgmode.el
@@ -246,12 +246,8 @@
   :url https://github.com/org-roam/org-roam-ui
   :straight (org-roam-ui :host github :repo "org-roam/org-roam-ui" :files ("*.el" "out"))
   :blackout (org-roam-ui-mode org-roam-ui-follow-mode)
-  ;; TODO
-  ;; `kill-buffer-save-emacs' confirm kill process
-  ;; befure running `kill-emacs-query-functions' and `kill-emacs-hook'
-  ;; we need to kill all org-roam-ui processes before `kill-buffer-save-emacs'
   :advice
-  (:before save-buffers-kill-terminal httpd-stop)
+  (:before save-buffers-kill-emacs httpd-stop)
   :setq
   (org-roam-ui-sync-theme      . t)
   (org-roam-ui-follow          . t)
