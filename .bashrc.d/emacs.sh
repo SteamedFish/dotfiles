a8f90adf25ef7c4bb50b3e6a06b1a8f7e8274a7b
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-10-15 17:40:23 +0800

    configure for emacs-vterm

--- .bashrc.d/emacs.sh
+++ .bashrc.d/emacs.sh
@@ -6,29 +6,39 @@ elif [ -d "$HOME/.emacs.d/bin" ]; then
     export PATH="$HOME/.emacs.d/bin:$PATH"
 fi
 
-# emacs-vterm
-if [ -n "$INSIDE_EMACS" ]; then
-    alias vi='emacsclient -n'
-    alias vim='emacsclient -n'
-    alias emacs="emacsclient -n"
-fi
-
 if [[ "$OSTYPE" == "linux-android" ]]; then
     return
 fi
 
-# https://github.com/akermu/emacs-libvterm#shell-side-configuration
-vterm_printf() {
-    if [ -n "$TMUX" ] && ([ "${TERM%%-*}" = "tmux" ] || [ "${TERM%%-*}" = "screen" ]); then
-        # Tell tmux to pass the escape sequences through
-        printf "\ePtmux;\e\e]%s\007\e\\" "$1"
-    elif [ "${TERM%%-*}" = "screen" ]; then
-        # GNU screen (screen, screen-256color, screen-256color-bce)
-        printf "\eP\e]%s\007\e\\" "$1"
-    else
-        printf "\e]%s\e\\" "$1"
+if [ "$INSIDE_EMACS" = "vterm" ]; then
+    alias vi='emacsclient -n'
+    alias vim='emacsclient -n'
+    alias emacs="emacsclient -n"
+
+    # https://github.com/akermu/emacs-libvterm#shell-side-configuration
+    vterm_printf() {
+        if [ -n "$TMUX" ] && ([ "${TERM%%-*}" = "tmux" ] || [ "${TERM%%-*}" = "screen" ]); then
+            # Tell tmux to pass the escape sequences through
+            printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+        elif [ "${TERM%%-*}" = "screen" ]; then
+            # GNU screen (screen, screen-256color, screen-256color-bce)
+            printf "\eP\e]%s\007\e\\" "$1"
+        else
+            printf "\e]%s\e\\" "$1"
+        fi
+    }
+
+    vterm_prompt_end() {
+        vterm_printf "51;A$(whoami)@$(hostname):$(pwd)"
+    }
+
+    if [ "$(basename "$SHELL")" = "zsh" ]; then
+        setopt PROMPT_SUBST
+        PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
+    elif [ -n "$BASH" ]; then
+        PS1=$PS1'\[$(vterm_prompt_end)\]'
     fi
-}
+fi
 
 # https://wiki.archlinux.org/index.php/Emacs#Multiplexing_emacs_and_emacsclient
 function emacs {
