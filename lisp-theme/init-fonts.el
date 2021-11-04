57c9c5aa33524c6771c80c02cfa3d5a678d68a06
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2021-11-04 23:55:11 +0800

    smaller NOTO CJK fonts

--- lisp-theme/init-fonts.el
+++ lisp-theme/init-fonts.el
@@ -86,6 +86,7 @@
       (setq buffer-face-mode-face 'my-align-by-sarasa)
       (make-variable-buffer-local 'face-font-rescale-alist)
       ;; make symbols smaller, 14 * 0.93 = 13 14 * 0.86 = 12, 14 * 0.79 = 11
+      (add-to-list 'face-font-rescale-alist '("-Noto Sans CJK SC-"    . 0.86))
       (add-to-list 'face-font-rescale-alist '("-STIXGeneral-"         . 0.86))
       (add-to-list 'face-font-rescale-alist '("-Apple Color Emoji-"   . 0.79))
       (buffer-face-mode))))
