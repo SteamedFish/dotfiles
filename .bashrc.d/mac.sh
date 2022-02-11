2cf0837a6229887ec782bb7c36da0401324c83a4
SteamedFish <69167+SteamedFish@users.noreply.github.com>
2022-02-11 15:50:07 +0800

    Config some homebrew options

--- .bashrc.d/mac.sh
+++ .bashrc.d/mac.sh
@@ -6,6 +6,12 @@ fi
 
 # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles
 
+export HOMEBREW_BAT=true
+export HOMEBREW_CURL_RETRIES=10
+export HOMEBREW_NO_AUTO_UPDATE=true
+export HOMEBREW_NO_INSTALL_CLEANUP=true
+export HOMEBREW_UPDATE_REPORT_ONLY_INSTALLED=true
+
 # envs from pass
 if [ -n "$(command -v pass)" ]; then
     HOMEBREW_GITHUB_API_TOKEN="$(pass env/homebrew)"
