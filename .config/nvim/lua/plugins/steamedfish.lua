31e92f252c8f6edbf652491e5c54c79647fef821
SteamedFish <steamedfish@hotmail.com>
2025-10-10 16:19:53 +0800

    add dot from extras

--- .config/nvim/lua/plugins/steamedfish.lua
+++ .config/nvim/lua/plugins/steamedfish.lua
@@ -21,14 +21,34 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "git_config",
-        "gitcommit",
-        "git_rebase",
-        "gitignore",
-        "gitattributes",
-      },
-    },
+    opts = function(_, opts)
+      local function add(lang)
+        if type(opts.ensure_installed) == "table" then
+          table.insert(opts.ensure_installed, lang)
+        end
+      end
+
+      vim.filetype.add({
+        extension = { rasi = "rasi", rofi = "rasi", wofi = "rasi" },
+        filename = {
+          ["vifmrc"] = "vim",
+        },
+        pattern = {
+          [".*/waybar/config"] = "jsonc",
+          [".*/mako/config"] = "dosini",
+          [".*/kitty/.+%.conf"] = "kitty",
+          [".*/hypr/.+%.conf"] = "hyprlang",
+          ["%.env%.[%w_.-]+"] = "sh",
+        },
+      })
+      vim.treesitter.language.register("bash", "kitty")
+      add("git_config")
+      add("hyprlang")
+      add("rasi")
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "shellcheck" } },
   },
 }
